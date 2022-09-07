9720d5bdad007654b2ad4bc4683cd7fbce72f456
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-09-06 17:59:43 -0700

    Base project for final demo created

---
+++ CNNUseCase Watch App/CNNUseCaseApp.swift
@@ -0,0 +1,21 @@
+//
+//  CNNUseCaseApp.swift
+//  CNNUseCase Watch App
+//
+//  Created by Mark Frazier on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyNewTestApp_Watch_AppApp: App {
+    @WKExtensionDelegateAdaptor(ExtensionDelegate.self) var extensionDelegate
+    
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                MenuView()
+            }
+        }
+    }
+}
