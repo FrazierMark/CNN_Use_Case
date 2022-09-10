1f5e8d8371b878649f7e1972d1f0922f08c54c06
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-09-09 18:20:27 -0700

    Add CoreView w/ Button Style

--- CNNUseCase Watch App/MenuView.swift
+++ CNNUseCase Watch App/MenuView.swift
@@ -17,37 +17,37 @@ struct MenuView: View {
                         }
                     }.listRowBackground(Color.red)
                     
-                    Section(header: Text("Edge")) {
-                        NavigationLink(destination: EdgeView().navigationBarTitle("Edge")) {
-                            Text("Edge")
-                        }
-                        
-                        NavigationLink(destination: ConsentView().navigationBarTitle("Consent")) {
-                            Text("Consent")
-                        }
-                        
-                        NavigationLink(destination: EdgeIdentityView().navigationBarTitle("Edge Identity")) {
-                            Text("Edge Identity")
-                        }
-                    }.listRowBackground(Color.red)
-                    
-                    Section(header: Text("Validation")) {
-                        NavigationLink(destination: AssuranceView().navigationBarTitle("Assurance")) {
-                            Text("Assurance")
-                        }
-                    }.listRowBackground(Color.red)
-                    
-                    Section(header: Text("Messaging")) {
-                        NavigationLink(destination: MessagingView().navigationBarTitle("Messaging")) {
-                            Text("Messaging")
-                        }
-                    }.listRowBackground(Color.red)
-                    
-                    Section(header: Text("Optimize")) {
-                        NavigationLink(destination: OptimizeView().navigationBarTitle("Optimize")) {
-                            Text("Optimize")
-                        }
-                    }.listRowBackground(Color.red)
+//                    Section(header: Text("Edge")) {
+//                        NavigationLink(destination: EdgeView().navigationBarTitle("Edge")) {
+//                            Text("Edge")
+//                        }
+//                        
+//                        NavigationLink(destination: ConsentView().navigationBarTitle("Consent")) {
+//                            Text("Consent")
+//                        }
+//                        
+//                        NavigationLink(destination: EdgeIdentityView().navigationBarTitle("Edge Identity")) {
+//                            Text("Edge Identity")
+//                        }
+//                    }.listRowBackground(Color.red)
+//                    
+//                    Section(header: Text("Validation")) {
+//                        NavigationLink(destination: AssuranceView().navigationBarTitle("Assurance")) {
+//                            Text("Assurance")
+//                        }
+//                    }.listRowBackground(Color.red)
+//                    
+//                    Section(header: Text("Messaging")) {
+//                        NavigationLink(destination: MessagingView().navigationBarTitle("Messaging")) {
+//                            Text("Messaging")
+//                        }
+//                    }.listRowBackground(Color.red)
+//                    
+//                    Section(header: Text("Optimize")) {
+//                        NavigationLink(destination: OptimizeView().navigationBarTitle("Optimize")) {
+//                            Text("Optimize")
+//                        }
+//                    }.listRowBackground(Color.red)
                 }.navigationBarTitle(Text("Extensions"))
                 
             }// .overlay(FloatingButtonView())
